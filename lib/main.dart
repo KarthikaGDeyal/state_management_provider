00dff8345d1900c60575d133bc72e235c23c8708
KarthikaGDeyal <karthikadeyal1999@gmail.com>
2022-12-06 21:19:12 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_manage.provid/screens/HomeScreen.dart';
+
+import 'provider/movie_provider.dart';
+
+
+void main() {
+  runApp(ChangeNotifierProvider<MovieProvider>(
+    child: MyApp(),
+    create: (_) => MovieProvider(), // Create a new ChangeNotifier object
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Provider Example',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const HomeScreenprovider(),
+    );
+  }
+}
